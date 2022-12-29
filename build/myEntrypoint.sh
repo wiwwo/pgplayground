56fb118c51e853e96b2f202b7c63c81826cd6f12
wiwwo <wiwwo+github@wiwwo.com>
2022-12-29 11:25:21 +0100

    Added console container, and ssh conection to hosts

--- build/myEntrypoint.sh
+++ build/myEntrypoint.sh
@@ -8,6 +8,19 @@ chown postgres: /var/lib/postgresql/.bash*
 
 WHAT_AM_I=${WHAT_AM_I:-primary}
 
+if [[ $WHAT_AM_I == 'console' ]]; then
+  ssh-keyscan -H console  >> ~/.ssh/known_hosts
+  ssh-keyscan -H pg_red   >> ~/.ssh/known_hosts
+  ssh-keyscan -H pg_green >> ~/.ssh/known_hosts
+  ssh-keyscan -H pg_blue  >> ~/.ssh/known_hosts
+  cp ~/.ssh/known_hosts /var/lib/postgresql/.ssh
+  chown postgres: /var/lib/postgresql/.ssh/known_hosts
+
+  exit 0
+
+fi
+
+
 if [[ $WHAT_AM_I == 'primary' ]]; then
   cp /etc/postgresql/15/main/postgresql.conf.primary     /etc/postgresql/15/main/postgresql.conf
 else
